dcd3c84c108f5f4b7e1846d87a843e448dbfee18
BintangSaputra <bintangcbs26@gmail.com>
2019-04-10 15:13:16 +0700

    add files

---
+++ index.js
@@ -0,0 +1,106 @@
+const names = [
+  "Dwiki Dharmawan",
+  "Monita Tahalea",
+  "Budi Doremi",
+  "Andre Hehanusa"
+];
+
+const showNames = names => {
+  for (let index = 0; index < names.length; index++) {
+    const name = names[index];
+
+    console.log(`[${index + 1}] ${name}`);
+  }
+};
+
+showNames(names);
+
+// filter Names
+
+const nameFull = [
+  "Dwiki Dharmawan",
+  "Monita Tahalea",
+  "Budi Doremi",
+  "Andre Hehanusa"
+];
+
+const filterNames = (names, minimumLength) => {
+  let newNames = [];
+
+  for (let index = 0; index < names.length; index++) {
+    const name = names[index];
+
+    if (name.length >= minimumLength) {
+      newNames.push(name);
+    }
+  }
+  return newNames;
+};
+
+const filteredNames = filterNames(nameFull, 12);
+
+showNames(filteredNames);
+
+// Array of Objects
+
+const myContacts = [
+  {
+    id: 1,
+    name: "Rudi Marudi",
+    phone: "+21883928192",
+    email: "Rudi123@raymail.com",
+    favorite: true,
+    rating: 9,
+    tags: ["popular", "cool"]
+  },
+  {
+    id: 2,
+    name: "Betty Lavenia",
+    phone: "+21234234234",
+    email: "bettykelapapeang@raymail.com",
+    favorite: false,
+    rating: 5,
+    tags: ["smart", "unpopular"]
+  },
+  {
+    id: 3,
+    name: "Fulcrum Boeing",
+    phone: "+21987836347",
+    email: "boeingoing@oing.org",
+    favorite: false,
+    rating: 6,
+    tags: ["strong", "can fly"]
+  }
+];
+
+// Show Contacts
+
+const showContacts = contacts => {
+  for (let index = 0; index < contacts.length; index++) {
+    const contact = contacts[index];
+    console.log(
+      `[${contact.id}] ${contact.name} (${contact.phone}) <${contact.email}>`
+    );
+  }
+};
+
+showContacts(myContacts);
+
+// Filter Contacts
+
+const filterContacts = (myContacts, minimumLength) => {
+  let newContacts = [];
+
+  for (let index = 0; index < myContacts.length; index++) {
+    const contact = myContacts[index];
+
+    if (contact.phone.length >= minimumLength) {
+      newContacts = contact;
+    }
+  }
+  return newContacts;
+};
+
+const filteredContacts = filterContacts(myContacts, 12);
+
+showContacts(filteredContacts);
